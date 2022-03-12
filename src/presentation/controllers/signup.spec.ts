d4294b2db87e66420f19c1d27b8471c9db9ab6ba
Thiago Fialho <thiago@provi.com.br>
2022-03-12 12:52:51 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,7 +3,6 @@ import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const httpRequest = {
-      name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
       passwordConfirmation: 'any_password'
@@ -14,5 +13,19 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no name is provided', () => {
+    const httpRequest = {
+      name: 'any_name',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+
+    const sut = new SignUpController()
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
