b0452d5c1f6a00dce3cd844013a1fbf2c1ac65ae
Thiago Fialho <thiago@provi.com.br>
2022-03-27 17:25:23 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
-export class MongoHelper {
+class MongoHelper {
   client: MongoClient
 
   async connect (uri: string): Promise<void> {
@@ -10,4 +10,10 @@ export class MongoHelper {
   async disconnect (): Promise<void> {
     await this.client.close()
   }
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
 }
+
+export default new MongoHelper()
