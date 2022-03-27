6c5329077470312966a8f16fc831319e432298c7
Thiago Fialho <thiago@provi.com.br>
2022-03-27 17:29:24 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,9 @@ class MongoHelper {
   client: MongoClient
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri)
+    this.client = await MongoClient.connect(uri, {
+      useUnifiedTopology: true
+    })
   }
 
   async disconnect (): Promise<void> {
