414e369a1797514e2af8567becfa5f4112cf916e
Thiago Fialho <thiago@provi.com.br>
2022-05-02 12:23:50 -0300

    feat: ensure LogMongoRepository creates an errorLog on success

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,7 +5,7 @@ import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -14,10 +14,6 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const controller = new SignUpController(emailValidator, dbAddAccount)
-
-  class FakeLogErrorRepository implements LogErrorRepository {
-    async log (stack: string): Promise<void> {}
-  }
-  const fakeLogErrorRepository = new FakeLogErrorRepository()
-  return new LogControllerDecorator(controller, fakeLogErrorRepository)
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(controller, logErrorRepository)
 }
