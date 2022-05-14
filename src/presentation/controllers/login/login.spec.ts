6c02fd9e297fd47517cb7334286047dcc71f1954
Thiago Fialho <thiago@provi.com.br>
2022-05-14 09:27:26 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { HttpRequest } from '../../protocols'
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
