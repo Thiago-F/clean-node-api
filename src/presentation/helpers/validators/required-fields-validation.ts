09eef7fb1d49070359a55a3c5db7df0536688324
Thiago Fialho <thiago@provi.com.br>
2022-05-18 01:09:58 -0300

    refactor: move required field validation to the ValidationComposite

---
+++ src/presentation/helpers/validators/required-fields-validation.ts
@@ -0,0 +1,16 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class RequiredFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: any): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
+
+    return null
+  }
+}
