9a3fe39edce1ee90a0cd6a8f4d4b66bebf148a8e
Thiago Fialho <thiago@provi.com.br>
2022-06-07 07:45:54 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,13 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
+
+  async auth ({ email, password }: AuthenticationModel): Promise<string | null> {
+    await this.loadAccountByEmailRepository.load(email)
+    return null
+  }
+}
