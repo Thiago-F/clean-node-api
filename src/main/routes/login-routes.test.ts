bdcb515d8ef252c9bbb60ef76aad5b0e5e987977
Thiago Fialho <thiago@provi.com.br>
2022-06-18 16:03:25 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
 import MongoHelper from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Login Routes', () => {
+  let accountCollection: Collection
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL ?? '')
   })
@@ -12,7 +14,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +27,27 @@ describe('Login Routes', () => {
           email: 'thiago@mail.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('[POST] /login', () => {
+    test('should return 200 on login', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Thiago',
+          email: 'thiago@mail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'thiago@mail.com',
+          password: '123456'
         })
         .expect(200)
     })
