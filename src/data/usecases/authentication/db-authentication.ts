dbec7ba42c682fff03440457dc1a55f139e9eb6f
Thiago Fialho <thiago@provi.com.br>
2022-06-18 10:35:59 -0300

    feat: ensure DbAuthentication calls updateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,12 +2,14 @@ import { Authentication, AuthenticationModel } from '../../../domain/usecases/au
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/cryptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth ({ email, password }: AuthenticationModel): Promise<string | null> {
@@ -20,6 +22,7 @@ export class DbAuthentication implements Authentication {
     const token = await this.tokenGenerator.generate(account.id)
     if (!token) return null
 
+    await this.updateAccessTokenRepository.update(account.id, token)
     return token
   }
 }
